a288694b9595ad2acdca19206f5f794990621b8e
adamw-git <adam_watson@sfu.ca>
2025-10-04 22:12:58 -0700

    made changes to DonatorHomeScreen

--- Android/app/src/main/java/com/example/donowaste/screens/donator/DonatorHomeScreen.kt
+++ Android/app/src/main/java/com/example/donowaste/screens/donator/DonatorHomeScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
